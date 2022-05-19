84ec4100b64e32db221c28c7989e51334e30f9e1
Shayane <shayaneteixeira56@gmail.com>
2022-05-19 18:46:22 -0300

    add 2 projects today

---
+++ board-hover/main.js
@@ -0,0 +1,36 @@
+const container = document.getElementById("container");
+const colors = ['#a80c0c', '#71bcdf', '#1cd364', '#7173df', '#ffa704', '#f1f15b'];
+const squares = 260;
+
+for (let i = 0; i < squares; i++) {
+    const square = document.createElement("div");
+
+    square.classList.add("square");
+    square.addEventListener(
+        "mouseover",
+        () => setColor(square
+        ));
+    square.addEventListener(
+        "mouseout",
+        () => removeColor(square
+        ));
+
+    container.appendChild(square);
+}
+
+function setColor(element) {
+    const color = getRandomColor();
+    element.style.background = color;
+    element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`;
+}
+
+function removeColor(element) {
+    element.style.background = "#1d1d1d";
+    element.style.boxShadow = "0 0 2px #000";
+}
+
+function getRandomColor() {
+    return colors[Math.floor(
+        Math.random() * colors.length
+    )];
+}
